6a1c8bc940c90981b3e3cc49c4e40dc1e552de17
raniachikhi <raniach0653@gmail.com>
2023-04-05 06:19:12 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -46,5 +46,5 @@ int check_palindrome(char *s, int l, int r)
 
 int is_palindrome(char *s)
 {
-	return (pal_checker(s, 0, _strlen_recursion(s) - 1));
+	return (check_palindrome(s, 0, _strlen_recursion(s) - 1));
 }
