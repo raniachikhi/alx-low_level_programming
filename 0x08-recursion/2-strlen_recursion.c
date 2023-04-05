bf7fd3d7cc16fbf95bdcde55d9db097a72437cce
raniachikhi <raniach0653@gmail.com>
2023-04-05 05:35:25 -0700

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,11 +8,13 @@
 
 int _strlen_recursion(char *s)
 {
-	int l = 0;
+	int i = 0;
+
 	if (*s)
 	{
-		l++;
-		l += _strlen_recursion(s + 1);
+		i++;
+		i += _strlen_recursion(s + 1);
+
 	}
-	return (l);
+	return (i);
 }
