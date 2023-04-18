b2b5af5e4c357d950c3d4f12a658d2ba2b0a94c2
raniachikhi <raniach0653@gmail.com>
2023-04-18 14:47:12 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,25 @@
+#ifndef STRUCT_DOG
+#define STRUCT_DOG
+
+/**
+  * struct dog- contains dog's info
+  * @name: first element
+  * @age: second element
+  * @owner : third element
+  */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+  * dog_tf- typedef for struct dog
+  */
+
+void init_dog(struct dog *d, char *name, float age, char *owner);void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
