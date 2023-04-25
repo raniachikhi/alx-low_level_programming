995f899052eaf50fe098a9e9246b2c10f60652f6
raniachikhi <raniach0653@gmail.com>
2023-04-25 03:49:28 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,11 +13,11 @@ dog_t *new_dog(char *name, float age, char *owner)
 	int l, m, n;
 	char *ptr, *qtr;
 
-	struct dog *nv_dog;
+	struct dog *new_dog;
 
 	l = 0, m = 0;
-	nv_dog = malloc(sizeof(struct dog));
-	if (nv_dog == NULL)
+	new_dog = malloc(sizeof(struct dog));
+	if (new_dog == NULL)
 		return (NULL);
 	while (*(name + l) != '\0')
 		l++;
@@ -26,13 +26,13 @@ dog_t *new_dog(char *name, float age, char *owner)
 	ptr = malloc(sizeof(char) * l + 1);
 	if (ptr == NULL)
 	{
-		free(nv_dog);
+		free(new_dog);
 		return (NULL);
 	}
 	qtr = malloc(sizeof(char) * m + 1);
 	if (qtr == NULL)
 	{
-		free(nv_dog);
+		free(new_dog);
 		free(ptr);
 		return (NULL);
 	}
@@ -40,8 +40,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 		ptr[n] = name[n];
 	for (n = 0; n <= m; n++)
 		qtr[n] = owner[n];
-	nv_dog->name = ptr;
-	nv_dog->age = age;
-	nv_dog->owner = qtr;
-	return (nv_dog);
+	new_dog->name = ptr;
+	new_dog->age = age;
+	new_dog->owner = qtr;
+	return (new_dog);
 }
