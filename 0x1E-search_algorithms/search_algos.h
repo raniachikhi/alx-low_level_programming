48d72f8b363b3c85e14a4291c7ba897981418670
raniachikhi <raniach0653@gmail.com>
2023-12-13 10:14:26 -0800

    add

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,9 @@
+#ifndef SEARCH_H
+#define SEARCH_H
+
+#include <stdio.h>
+
+/* Prototypes of all functions */
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+#endif
